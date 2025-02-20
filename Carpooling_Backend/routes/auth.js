cb897c90e8b0367f25f2dd50d3f4c07e84f45277
Swarajhere <swaraj.nalawade2004@gmail.com>
2025-02-20 12:06:37 +0530

    m

--- Carpooling_Backend/routes/auth.js
+++ Carpooling_Backend/routes/auth.js
@@ -3,7 +3,6 @@ const router = express.Router(); // Creating a router
 const { signup, login, changePassword, verifyEmail } = require('../controllers/loginSignUpController');
 const upload = require("../utils/multer"); 
 const authMiddleware = require('../middlewares/auth-middleware'); 
-const upload = require("../utils/multer");
 
 
 router.post('/signup', upload.single("profilePicture"), signup); 
