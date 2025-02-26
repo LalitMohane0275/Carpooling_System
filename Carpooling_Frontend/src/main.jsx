36fc72b9bedb19b479ff5240a79ffb2358457f42
LalitMohane0275 <lalitmohane0275.com>
2025-02-26 17:21:17 +0530

    scroll on top

--- Carpooling_Frontend/src/main.jsx
+++ Carpooling_Frontend/src/main.jsx
@@ -1,13 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import ScrollToTop from './components/ScrollToTop';
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </Provider>
